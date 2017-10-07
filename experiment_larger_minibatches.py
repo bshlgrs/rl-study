b284e176248c15591f95c027637db94b906d669a
Buck Shlegeris <bshlegeris@gmail.com>
2017-10-07 16:55:42 -0700

    using github as a file sync service

--- experiment_larger_minibatches.py
+++ experiment_larger_minibatches.py
@@ -4,7 +4,7 @@ from run_dqn_atari import *
 def main():
     env = get_env(3)
     session = get_session()
-    agent = DQNAgent(env, session, batch_size=256)
+    agent = DQNAgent(env, session, batch_size=512)
 
     agent.learn(1000001)
     env.close()
