c079769da3734353ef653e8b67ebe00860f1e735
Buck Shlegeris <bshlegeris@gmail.com>
2017-10-09 19:54:53 -0700

    the previous commit definitely works

--- experiment.py
+++ experiment.py
@@ -1,7 +1,10 @@
 from run_dqn_atari import *
-
+import os
 
 def main():
+    print("your git commit is ")
+    os.system("git rev-parse HEAD")
+
     env = get_env(3)
     session = get_session()
     agent = DQNAgent(env, session, batch_size=32)
