379fdde2506e4763ff9becc5766e2b98241e3a59
Buck Shlegeris <bshlegeris@gmail.com>
2017-10-10 13:12:52 -0700

    make it play Beam Rider

--- A3C.py
+++ A3C.py
@@ -1,6 +1,14 @@
+from tensorflow.contrib import layers
+
 import models
 from dqn_utils import *
 import gym.spaces
+
+
+class Environment:
+    def __init__(self):
+        pass
+
 
 
 class A3CModel(object):
@@ -8,26 +16,55 @@ class A3CModel(object):
         self.session = session
         self.env = env
         self.num_actions = env.action_space.n
-
+        img_h, img_w, img_c = env.observation_space.shape
+        self.input_shape = (img_h, img_w, self.frame_history_len * img_c)
         self.frame_history_len = 3
-
         self.conv_function = conv_function or models.atari_convnet
 
-        self.obs_t_ph = tf.placeholder(tf.uint8, [None] + list(self.input_shape), name='obs_t_ph')
-        self.act_t_ph = tf.placeholder(tf.int32, [None], name='act_t_ph')
-        self.rew_t_ph = tf.placeholder(tf.float32, [None], name='rew_t_ph')
-        self.obs_tp1_ph = tf.placeholder(tf.uint8, [None] + list(self.input_shape), name='obs_tp1_ph')
-        self.done_mask_ph = tf.placeholder(tf.float32, [None], name='done_mask_ph')
+        obs_t_ph = tf.placeholder(tf.uint8, [None] + list(self.input_shape), name='obs_t_ph')
+        act_t_ph = tf.placeholder(tf.int32, [None], name='act_t_ph')
+        rew_t_ph = tf.placeholder(tf.float32, [None], name='rew_t_ph')
+        # obs_tp1_ph = tf.placeholder(tf.uint8, [None] + list(self.input_shape), name='obs_tp1_ph')
+        done_mask_ph = tf.placeholder(tf.float32, [None], name='done_mask_ph')
         self.model_initialized = False
 
         self.learning_rate_ph = tf.placeholder(tf.float32, (), name="learning_rate_ph")
 
         num_actions = self.num_actions
-        q_func = self.q_func
+        conv_function = self.conv_function
 
         # casting to float on GPU ensures lower data transfer times. TODO: understand this better
-        obs_t_float = tf.cast(self.obs_t_ph, tf.float32) / 255.0
-        obs_tp1_float = tf.cast(self.obs_tp1_ph, tf.float32) / 255.0
+        obs_t_float = tf.cast(obs_t_ph, tf.float32) / 255.0
+
+        conv_layer = conv_function(obs_t_float)
+
+        with tf.variable_scope('policy'):
+            policy_fc1 = layers.fully_connected(conv_layer, num_outputs=256, activation_fn=tf.nn.relu)
+            policy_fc2 = layers.fully_connected(policy_fc1, num_outputs=num_actions, activation_fn=None)
+            policy_out = tf.nn.softmax(policy_fc2)
+
+        with tf.variable_scope('value'):
+            value_fc1 = layers.fully_connected(conv_layer, num_outputs=256, activation_fn=tf.nn.relu)
+            value_fc2 = layers.fully_connected(value_fc1, num_outputs=256, activation_fn=None)
+            value_out = tf.nn.softmax(value_fc2)
+
+        one_hot_policy = act_t_ph * policy_out
+        log_policy = tf.log(tf.reduce_sum(one_hot_policy, axis=1, keep_dims=True) + 1e-10)
+        advantage = rew_t_ph - value_out
+
+        value_loss_constant = 0.5
+        regularization_constant = 0.01
+
+        loss_policy = -log_policy * tf.stop_gradient(advantage)
+        loss_value = value_loss_constant * tf.square(advantage)
+
+        entropy = regularization_constant * tf.reduce_sum(tf.log(policy_out + 1e-10) * policy_out, axis=1,
+                                                          keep_dims=True)
+
+        loss_total = tf.reduce_mean(loss_policy + loss_value - entropy)
+        optimizer = tf.train.RMSPropOptimizer(self.learning_rate_ph, decay=.99)
+        minimize = optimizer.minimize(loss_total)
+
 
 
 
@@ -49,7 +86,6 @@ class A3CAgent:
         self.max_episode_timesteps = max_episode_timesteps
         self.model = A3CModel(conv_function=conv_function)
         self.gamma = 0.99
-
 
         assert type(self.env.observation_space) == gym.spaces.Box
         assert type(self.env.action_space) == gym.spaces.Discrete
--- experiment.py
+++ experiment.py
@@ -7,11 +7,12 @@ def main():
     print("your git commit is ")
     os.system("git rev-parse HEAD")
 
-    env = get_env(3)
+    # options are: Beam Rider, Breakout, Enduro, Pong, Qbert, Seaquest, Space Invaders
+    env = get_env(0)
     session = get_session()
-    agent = DQNAgent(env, session, batch_size=512, q_func=models.duelling_atari_model)
+    agent = DQNAgent(env, session, batch_size=512, q_func=models.atari_model)
 
-    agent.learn(1000001)
+    agent.learn(2000001)
     env.close()
 
 
