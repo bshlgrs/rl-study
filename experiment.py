7f13af010220334510f18a8f3f0ad8e646351f07
Buck Shlegeris <bshlegeris@gmail.com>
2017-10-09 20:04:02 -0700

    using github as a file sync service

--- experiment.py
+++ experiment.py
@@ -1,16 +1,18 @@
 from run_dqn_atari import *
 import os
 
+
 def main():
     print("your git commit is ")
     os.system("git rev-parse HEAD")
 
     env = get_env(3)
     session = get_session()
-    agent = DQNAgent(env, session, batch_size=32)
+    agent = DQNAgent(env, session, batch_size=512)
 
     agent.learn(500001)
     env.close()
+
 
 if __name__ == '__main__':
     main()
